28c989b379474f1769ff07dd0dd8314de0b43612
shuchuanbiao <shuchuanbiao@yuantutech.com>
2021-09-01 14:51:38 +0800

    test URL

--- src/main/java/com/cloud/test/URLtest.java
+++ src/main/java/com/cloud/test/URLtest.java
@@ -8,6 +8,11 @@ public class URLtest {
         String url = "xxxxx?param=" + URLEncoder.encode("za4T8MHB/6mhmYgXB7IntyyOUL7Cl++0jv5rFxAIFVji8GDrcf+k8g==", "utf-8");
         System.out.println(url);
         url = URLDecoder.decode(url, "utf-8");
+        String myUrl = URLDecoder.decode("https%3A%2F%2Fuat.yuantutech.com%2Fyuantu%2Fh5-cli%2F1.21." +
+                "7%2Fpay-status.html%3FcorpId%3D929%26unionId%3D3601%26feeChannel%3D6%26optType%3D1&tr" +
+                "adeType=APP&feeChannel=6&optType=1&corpId=929&unionId=3601&t=85068&invokerChannel=H5&i" +
+                "nvokerDeviceType=others&invokerAppVersion=2.22.4&callback=jsonp1630466687515", "utf-8");
+        System.out.println(myUrl);
         System.out.println(url);
     }
 }
